637ca7a4c10eeb32ba34c235ff33671dca9782cf
rubayetanjumjoy <makarov@misnadia.aci-bd.local>
2023-11-06 12:36:01 +0600

    api

--- temperature_map/views.py
+++ temperature_map/views.py
@@ -13,23 +13,27 @@ class WeatherDataList(APIView):
     # permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        year = self.request.query_params.get('year', None)
-        month = self.request.query_params.get('month', None)
-        day = self.request.query_params.get('day', None)
-        station_name = self.request.query_params.get('station_name', None)
+        year = self.request.query_params.get("year", None)
+        month = self.request.query_params.get("month", None)
+        day = self.request.query_params.get("day", None)
+        station_name = self.request.query_params.get("station_name", None)
+        data_type = self.request.query_params.get("data_type", None)
 
         weather_data = WeatherData.objects.all()
         if year:
             weather_data = weather_data.filter(c_year=year)
-        
+
         if month:
             weather_data = weather_data.filter(c_month=month)
-        
+
         if day:
             weather_data = weather_data.filter(c_day=day)
-        
+
         if station_name:
             weather_data = weather_data.filter(station_name=station_name)
+
+        if data_type:
+            weather_data = weather_data.filter(data_type=data_type)
 
         serializer = WeatherDataSerializer(weather_data, many=True)
         return Response(serializer.data)
@@ -39,9 +43,28 @@ class WeatherDataNewList(APIView):
     # permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        year = self.request.query_params.get('year', 2023)
-        month = self.request.query_params.get('month', 1)
+        year = self.request.query_params.get("year", None)
+        month = self.request.query_params.get("month", None)
+        station = self.request.query_params.get("station_name", None)
+        data_type = self.request.query_params.get("data_type", None)
+        day = self.request.query_params.get("day", None)
 
-        weather_data = WeatherDataNew.objects.filter(c_month=month, c_year=year)
+        weather_data = WeatherDataNew.objects.all()
+        if year:
+            weather_data = weather_data.filter(c_year=year)
+
+        if month:
+            weather_data = weather_data.filter(c_month=month)
+
+        if day:
+            weather_data = weather_data.filter(c_day=day)
+
+        if station:
+            weather_data = weather_data.filter(station=station)
+
+        if data_type:
+            weather_data = weather_data.filter(data_type=data_type)
+
+        # weather_data = WeatherDataNew.objects.filter(c_month=month, c_year=year)
         serializer = WeatherDataNewSerializer(weather_data, many=True)
         return Response(serializer.data)
