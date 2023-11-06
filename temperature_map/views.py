87a2c3de6253e037802cdf212568760f21621d8d
shaonsust <shaon63@gmail.com>
2023-11-06 19:37:17 +0600

    weather prediction average api complete

--- temperature_map/views.py
+++ temperature_map/views.py
@@ -1,5 +1,8 @@
+from audioop import avg
+from datetime import datetime
 from django.shortcuts import render
 from django.http import request
+from django.db.models import Avg
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -49,7 +52,10 @@ class WeatherDataPredictionAverage(APIView):
     # permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        year = self.request.query_params.get("year", None)
+        curr_year = datetime.now().year
+        curr_month = datetime.now().month
+
+        year = self.request.query_params.get("year", datetime.now().year)
         month = self.request.query_params.get("month", None)
         station = self.request.query_params.get("station", None)
         data_type = self.request.query_params.get("data_type", None)
@@ -58,7 +64,8 @@ class WeatherDataPredictionAverage(APIView):
 
         weather_data = WeatherDataPrediction.objects.all()
         if year:
-            weather_data = weather_data.filter(c_year=year)
+            weather_data = weather_data.filter(
+                c_year__in=[year, int(year) + 1])
 
         if month:
             weather_data = weather_data.filter(c_month=month)
@@ -71,13 +78,73 @@ class WeatherDataPredictionAverage(APIView):
 
         if data_type:
             weather_data = weather_data.filter(data_type=data_type)
-        
+
         if source:
             weather_data = weather_data.filter(source=source)
 
-        # weather_data = WeatherDataNew.objects.filter(c_month=month, c_year=year)
-        serializer = WeatherDataPredictionSerializer(weather_data, many=True)
-        return Response(serializer.data)
+        weather_data = weather_data.values(
+            'data_type',
+            'station',
+            'c_year',
+            'c_month'
+        ).annotate(
+            avg_value=Avg('value')
+        ).order_by('station', 'c_year', 'c_month',)
+
+        result_dict = {}
+        for value in weather_data:
+            station = value['station']
+            month = value['c_month']
+            year = value['c_year']
+            data_type = value['data_type']
+
+            if year == curr_year and month >= curr_month:
+                if station not in result_dict:
+                    result_dict[station] = {}
+
+                if year not in result_dict[station]:
+                    result_dict[station][year] = {}
+
+                if month not in result_dict[station][year]:
+                    result_dict[station][year][month] = {}
+
+                if data_type not in result_dict[station][year][month]:
+                    result_dict[station][year][month][data_type] = {}
+
+                result_dict[station][year][month][data_type] = value
+
+            if year > curr_year:
+                if station not in result_dict:
+                    result_dict[station] = {}
+
+                if year not in result_dict[station]:
+                    result_dict[station][year] = {}
+
+                if month not in result_dict[station][year]:
+                    result_dict[station][year][month] = {}
+
+                if data_type not in result_dict[station][year][month]:
+                    result_dict[station][year][month][data_type] = {}
+
+                result_dict[station][year][month][data_type] = value
+
+        result = {}
+        for station in result_dict:
+            for year in result_dict[station]:
+                for month in result_dict[station][year]:
+                    if station not in result:
+                        result[station] = []
+                    
+                    # result[station].append(result_dict[station][year][month])
+                    result[station].append({
+                        'station': station,
+                        'year': year,
+                        'month': month,
+                        'temperature_avg_value': result_dict[station][year][month]['temperature']['avg_value'],
+                        'humidity_avg_value': result_dict[station][year][month]['humidity']['avg_value'],
+                    })
+
+        return Response(result)
 
 
 class WeatherDataPredictionDetails(APIView):
@@ -106,7 +173,7 @@ class WeatherDataPredictionDetails(APIView):
 
         if data_type:
             weather_data = weather_data.filter(data_type=data_type)
-        
+
         if source:
             weather_data = weather_data.filter(source=source)
 
@@ -141,7 +208,7 @@ class WeatherDataHistoricalDetails(APIView):
 
         if data_type:
             weather_data = weather_data.filter(data_type=data_type)
-        
+
         if source:
             weather_data = weather_data.filter(source=source)
 
