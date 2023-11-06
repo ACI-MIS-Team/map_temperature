0c8e20fae9de8aac624ce0b7687f360c13db1776
shaonsust <shaon63@gmail.com>
2023-11-06 14:15:04 +0600

    api renamed

--- temperature_map/urls.py
+++ temperature_map/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.MapView.as_view(), name="map_view"),
     path("weather_data/", views.WeatherDataList.as_view(), name='weather-data-list'),
-    path("weather_data_prediction-average/", views.WeatherDataPredictionAverage.as_view(), name='weather-data-prediction-average'),
+    path("weather_data_prediction_average/", views.WeatherDataPredictionAverage.as_view(), name='weather-data-prediction-average'),
     path("weather_data_prediction_details/", views.WeatherDataPredictionDetails.as_view(), name='weather-data-prediction-details'),
     path("weather_data_historical_details/", views.WeatherDataHistoricalDetails.as_view(), name='weather-data-new-list'),
 ]
